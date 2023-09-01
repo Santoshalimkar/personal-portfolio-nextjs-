28ca4f9c2e07ccfaa1acb04c964510319be3cccb
santosh alimkar <santosh.sa4d@gmail.com>
2023-09-01 19:41:56 +0530

    fourth commit

--- app/Footer/page.js
+++ app/Footer/page.js
@@ -2,12 +2,10 @@
 import React from 'react'
 import Image from 'next/image'
 import perpic from "../../public/perpic.jpeg"
-import { ToggleSidebar } from '../Components/Sidebar'
 
 function Footer() {
-  const {toggle,settoggle}=ToggleSidebar()
   return (
-    <div className={ toggle?'blurback':'bg-[#ecf0f3] flex justify-center items-center w-full h-40 gap-4 mx-auto flex-col'}>
+    <div className='bg-[#ecf0f3] flex justify-center items-center w-full h-40 gap-4 mx-auto flex-col'>
         <div>
         <Image  className='border-2 bg-slate-300  p-1 rounded-full h-14 w-14 '  src={perpic} alt='perpic' />
         </div>
