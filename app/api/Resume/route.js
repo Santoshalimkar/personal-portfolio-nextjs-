2db67bbe52932b7054264ed916e7556ec83e1f25
santosh alimkar <santosh.sa4d@gmail.com>
2023-09-28 15:47:54 +0530

    latest2  commit

--- app/api/Resume/route.js
+++ app/api/Resume/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { connectDB, client } from "../../Utils/Mongoconn";
 
-export default async function getData() {
+export default async function GetData() {
   const db = client.db();
   const data = await db.collection("portfolioDetails").find().toArray();
   return data;
@@ -11,7 +11,7 @@ export async function GET(req, res) {
   await connectDB();
   try {
     if (req.method === "GET") {
-      const resumedata = await getData();
+      const resumedata = await GetData();
       return NextResponse.json(resumedata);
     }
   } catch (error) {
